e96cfeb3fcd4711b62a61053f207c8f7bbb1726d
Maxim Chadliev <chad0441@gmail.com>
2018-07-24 17:45:43 -0400

    iteration 1-3 done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,51 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+  foods: any[];
+  foodsSearched: String = "";
+  filteredFood: Array<any> = [];
+
+  newFood: Object = {};
+  listFood: Object = {};
 
   constructor() { }
 
   ngOnInit() {
+    this.foods = foods;
+    this.filteredFood = foods;
+  
+  }
+  
+  searchItems(){ 
+    this.filteredFood = this.foods.filter(singleFood => {
+      return singleFood.name.toLowerCase().includes(this.foodsSearched.toLowerCase())
+    });
+     console.log(this.filteredFood)
   }
 
+  addFood() {
+    this.foods.push(this.newFood);
+  
+  this.newFood= {
+    name: "",
+    calories: "",
+    image: "",
+    quantity: ""
+  }
 }
+
+  addToList() {
+    this.foods.push(this.listFood);
+
+    this.listFood= {
+      name: "",
+      calories: "",
+    }
+  }
+  
+
+
+  
+}
+  
+
